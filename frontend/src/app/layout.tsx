e70441c5b00633ebdddc03e2b5c5695157e6af14
Lakshay Malhotra <lakshay.m2976@gmail.com>
2026-06-12 18:36:41 +0530

    Add font-display:swap and preconnect hints for Material Symbols CDN

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -57,11 +57,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${dmSerifDisplay.variable} ${manrope.variable} ${spaceGrotesk.variable}`}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
         />
       </head>
       <body className="font-body antialiased bg-background text-on-background transition-colors duration-300">
